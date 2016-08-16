84244678821d517387410e0802c5b4a73aa60a18
Luis Navarro <luis.navarro@pricetravel.com>
2016-08-16 17:34:16 -0500

    for stash

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var uglify = require ('uglifyify');
+var source = require('vinyl-source-stream');
+
+gulp.task("build",function(){
+	var options={
+		entries:'scripts.app.js',
+		transform[]
+	};
+	options.transform.push([uglify,{global:true}]);
+
+	var bunder = browserify(options);
+	bundler.bundle()
+	.pipe(source('./scripts/app.js'))
+	.pipe(gulp.dest('./scripts/public/'));
+})
