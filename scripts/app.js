0d8157bf926dfb46943e12863359019fd264d824
Julio Carlos Santana Ruiz <julio.santana@pricetravel.com>
2016-08-16 16:54:11 -0500

    Node functions...

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,5 @@
+var angular = require('angular');
 var danielaAndradeApp = angular.module('danielaAndradeApp', []);
-//agregar popup para video
-//agregar html de track como template y con browserify agregarlos al js final
-//hacer ejemplo directo con browserify y despues con gulp
-//separar css
-danielaAndradeApp.controller('TrackListController', function TrackListController($scope) {
-   //Traer los track desde un servicio
-   $scope.tracks = [
-      {
-         chapter: 1,
-         title: 'Digital Age',
-         backgroundClass: 'digitalAge',
-         video: 'https://www.youtube.com/embed/eeOeZjK4D98',
-         available: true
-      },
-      {
-         chapter: 2,
-         title: 'Sound',
-         backgroundClass: 'sound',
-         video: 'https://www.youtube.com/embed/xqf2DJgucsU',
-         available: true
-      },
-      {
-         chapter: 3,
-         title: 'Come Around',
-         backgroundClass: 'comeAround',
-         video: 'https://www.youtube.com/embed/I-fL4KJh_XQ',
-         available: true
-      },
-      {
-         chapter: 4,
-         title: 'Shore',
-         backgroundClass: 'shore',
-         video: '',
-         available: false
-      },
-   ];
-});
+
+require('./components');
+require('./services');
