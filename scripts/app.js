9e939ea8a0d52d778a0bc833eb8f62de7571f3f5
Jose Manuel Pereira Cih <jose.pereira@pricetravel.com>
2016-08-16 17:04:59 -0500

    no message

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,6 @@ var danielaAndradeApp = angular.module('danielaAndradeApp', []);
 //agregar popup para video
 //agregar html de track como template y con browserify agregarlos al js final
 //hacer ejemplo directo con browserify y despues con gulp
-//separar css
 danielaAndradeApp.controller('TrackListController', function TrackListController($scope) {
    //Traer los track desde un servicio
    $scope.tracks = [
