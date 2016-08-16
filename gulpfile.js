dff568f7fc7a92ceff9bc8109444d4efe93820be
Juan José Portillo Gutiérrez <juan.portillo@pricetravel.com>
2016-08-16 17:08:54 -0500

    Saved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var uglify = require('uglifyjs');
+var source = require('vinyl-source-stream');
+
+gulp.task("build", function(){
+	var options={
+		entries: 'scripts.app.js'
+		transform: []
+	};
+	options.transform.push([uglify,{global:true}]);
+
+var bundler = browserify(options);
+bundler.bundle()
+.pipe(source('./scripts/app.js'))
+.pipe(gulp.dest('./scripts/public'))
+
+
+})
