dba07611f92384de5caaf0b83877c7d8739823a7
Marco Antonio Flores Robles <marco.flores@pricetravel.com.mx>
2016-08-24 21:10:42 -0500

    Creaciones generales

---
+++ proyectpelicula/modules/MoviesHelper/index.js
@@ -0,0 +1,131 @@
+var q = require("q");
+var request = require("./requester");
+
+/**
+  options = {
+      query: '', 
+      title:'',
+      page:2,
+      year:1997,
+      type:"Movie"
+  } 
+ */
+
+var finder = {
+    search : function(options){
+            //s=popo&plot=short&r=json&page=6&type=movie
+            //t=popo
+         var self = this;
+         var mapper = {
+                query: 's', 
+                title:'t',
+                page:'page',
+                year:'y',
+                type:'type',
+                season:'Season'
+            }
+
+
+            if(typeof options != 'object'){
+                throw new Error('Option type Error');
+            }
+
+            var queryString = "?"
+
+           for(key in options){
+               if(options.hasOwnProperty(key) && mapper.hasOwnProperty(key)){
+                   if(key == 'type' && options[key] != self.TYPES.MOVIE
+                   &&  options[key] != self.TYPES.SERIE
+                   &&  options[key] != self.TYPES.EPISODE){
+                       continue; 
+                   }
+                   queryString += mapper[key] + '=' +options[key]+'&';
+               }
+           }
+
+            return request({url: queryString});
+    },
+    getDetail : function(id,plotSize){
+        //i=tt0787524&tomatoes=true
+        if(typeof id != 'string'){
+            throw new Error('Invalid imdbID');
+        }
+       
+        var queryString = '?tomatoes=true&i='+id+'&';
+        if(plotSize == this.PLOTSIZE.SHORT || this.PLOTSIZE.FULL){
+            queryString += 'plot='+plotSize;
+        }
+
+        return request({url: queryString});
+    },
+    getRandomSearch : function(){
+        var feed = 'abcdefghijklmnopqrsstuv';
+        var randomSearch = '';
+
+        for(i = 0; i < 2; i++){
+            randomSearch += feed[(Math.ceil(Math.random()*100))%feed.length];
+        }
+
+        return this.search({query : randomSearch});
+        
+    },
+	getRandomSearchMovie : function(){
+        var feed = 'abcdefghijklmnopqrsstuv';
+        var randomSearch = '';
+
+        for(i = 0; i < 2; i++){
+            randomSearch += feed[(Math.ceil(Math.random()*100))%feed.length];
+        }
+
+        var busqueda = this.search({query : randomSearch,type:'movie'});
+		if(busqueda == undefined)
+		{
+			return this.search({query : randomSearch});
+		}
+        else
+		{
+			return busqueda;
+		}
+        
+    },
+	getRandomSearchSeries : function(){
+        var feed = 'abcdefghijklmnopqrsstuv';
+        var randomSearch = '';
+
+        for(i = 0; i < 2; i++){
+            randomSearch += feed[(Math.ceil(Math.random()*100))%feed.length];
+        }
+		var busqueda = this.search({query : randomSearch,type:'series'});
+		if(busqueda == undefined)
+		{
+			return this.search({query : randomSearch});
+		}
+        else
+		{
+			return busqueda;
+		}
+        
+    }
+};
+
+Object.defineProperties(finder,{
+    TYPES:{
+        writable: false, 
+        value:{
+            MOVIE:'movie',
+            SERIE: 'series',
+            EPISODE: 'episode'
+        } 
+    },
+    PLOTSIZE:{
+        writable:false,
+        value:{
+            SHORT:'short',
+            FULL:'full'
+        }
+    }
+ });
+
+
+
+ module.exports = finder;
--- proyectpelicula/routes/index.js
+++ proyectpelicula/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var movieHelper = require('../modules/MoviesHelper');
+var options = {
+	query:'Dracula',
+	year:2000
+}
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+
+router.get('/', function(req, res, next) {
+	
+	movieHelper.getRandomSearchMovie().then(function(a){
+	movieHelper.search({title:'Big Hero 6'}).then(function(b){
+		movieHelper.getRandomSearchSeries().then(function(c){
+	  res.render('index', { title: 'Express',movies:a ,movie:b ,series:c});
+			});
+		});	
+	});
+});
+
+/* GET review page. */
+router.get('/reviews.html', function(req, res, next) {
+	
+	movieHelper.getRandomSearchMovie().then(function(a){
+	movieHelper.search({title:'Big Hero 6'}).then(function(b){
+		movieHelper.getRandomSearchSeries().then(function(c){
+	  res.render('index', { title: 'Express',movies:a ,movie:b ,series:c});
+			});
+		});	
+	});
 });
 
 module.exports = router;
