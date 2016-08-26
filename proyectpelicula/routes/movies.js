b04ddb4f33b09a44c9d14ee2604fb0f9cb7bd7c0
Marco Antonio Flores Robles <marco.flores@pricetravel.com.mx>
2016-08-25 22:25:37 -0500

    Actualizacion del angular

--- proyectpelicula/routes/movies.js
+++ proyectpelicula/routes/movies.js
@@ -10,4 +10,13 @@ router.get('/movies', function(req, res, next) {
 	});
 });
 
+router.post('/movies/:title', function(req, res) {
+	movieHelper.search({query:req.params.title}).then(function(a){
+		console.log(a)
+		res.json(a);
+	}, function(error){
+	res.json(error);
+	});
+});
+
 module.exports = router;
